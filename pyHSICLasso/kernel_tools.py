edc117998a6ddb77cba4edfee6101ce0911b9884
teyden <teydenn@gmail.com>
2020-03-12 21:20:07 -0700

    Print out kernels

--- pyHSICLasso/kernel_tools.py
+++ pyHSICLasso/kernel_tools.py
@@ -23,6 +23,7 @@ def kernel_delta_norm(X_in_1, X_in_2):
         ind_1 = np.where(X_in_1 == ind)[1]
         ind_2 = np.where(X_in_2 == ind)[1]
         K[np.ix_(ind_1, ind_2)] = 1 / c_1 / c_2
+    print(K)
     return K
 
 
@@ -48,6 +49,7 @@ def kernel_gaussian(X_in_1, X_in_2, sigma):
     X_in_22 = np.sum(np.power(X_in_2, 2), 0)
     dist_2 = np.tile(X_in_22, (n_1, 1)) + \
         np.tile(X_in_12, (n_2, 1)).transpose() - 2 * np.dot(X_in_1.T, X_in_2)
+    print(dist_2)
     K = np.exp(-dist_2 / (2 * np.power(sigma, 2)))
     return K
 
